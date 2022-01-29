748c0862b0f437e092f7b066625de1f7138c6968
NikhilBajpai <nikhil.cse16@gmail.com>
2022-01-29 20:04:37 +0530

    day 1 bfs Traversal

--- bfsTraversal.py
+++ bfsTraversal.py
@@ -1,3 +1,24 @@
 
 
+def breadthFirstTraversal(graph , startingNode):
 
+    queue = [startingNode]
+    res = ""
+    while len(queue)>0:
+        top =  queue.pop(0)
+        res+=top
+        for neighbour in graph[top]:
+            queue.append(neighbour)
+
+    return (res)
+
+graph = {
+    "a": ["c", "b"],
+    "b": ["d"],
+    "c": ["e"],
+    "d": ["f"],
+    "e": [],
+    "f": []
+}
+
+print(breadthFirstTraversal(graph , "a"))
